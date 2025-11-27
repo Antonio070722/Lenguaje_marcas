b74d3fd731573db43b00de77a445e9d6376c6c89
Antonio Llorente <allopal2911@g.educaand.es>
2025-11-27 19:20:38 +0100

    CV JavaScript/index.html POR HACER

---
+++ Ejercicios/CV JavaScript/script.js
@@ -0,0 +1,11 @@
+function cambiarNombre() {
+    const input = document.querySelector('#nombre-input')
+    const nombre = document.querySelector('#nombre')
+
+    const nuevoNombre = input.value
+    nombre.textContent = nuevoNombre
+}
+
+function cambiarFondo(){
+    
+}
